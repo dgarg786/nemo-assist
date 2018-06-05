cb6e70a15071d9a786df77799e4dc9e9357b7917
Deepanshu <deegarg@paypal.com>
2018-06-05 21:08:37 +0530

    update Comments and readme

--- src/lib/components/textInput.js
+++ src/lib/components/textInput.js
@@ -1,5 +1,5 @@
-import {resolve,reject} from '../promiseHandler';
-import {normalizedLocator} from '../common';
+import { resolve, reject } from '../promiseHandler';
+import { normalizedLocator } from '../common';
 import q from 'q';
 import _ from 'lodash';
 
@@ -8,14 +8,14 @@ import _ from 'lodash';
  * @description returns a promise function which waits for the TextInput and then enters the required text
  * @param {object} nemo nemo object
  * @param {string} optionToEnter Text to enter in TextInput
- * @param {object|string} textInputLocator  a DOM locator that can be either location of locator in LOCATOR folder eg: 'policy.spinner'<br>
+ * @param {object|string} textInputLocator  a DOM locator that can be either string with location of nemo-view object in LOCATOR folder eg: 'policy.spinner'<br>
                              OR can be direct locator object eg: { "type": "css", "locator": "div.hasSpinner"}<br>
                              OR can be locator string eg 'xpath://*[@name = "documentType"]'
- * @param {number} waitTime waitTime in milliseconds by default it's 3000
+ * @param {number} waitTime waitTime in milliseconds by default it's 20000
  * @returns {Function}
  */
 export default function textInputEnter(nemo, optionToEnter, textInputLocator, waitTime) {
-    waitTime = waitTime || _.get(nemo, 'data.waitTime', 3000);
+    waitTime = waitTime || _.get(nemo, 'data.waitTime', 20000);
     return function () {
         const locator = normalizedLocator(nemo, textInputLocator);
         const defer = q.defer();
