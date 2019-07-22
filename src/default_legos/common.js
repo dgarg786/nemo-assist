e7961421f1917f3c0632c279ef8f8fea742bd6b9
Deepanshu <deegarg@paypal.com>
2019-07-22 17:22:40 +0530

    0.0.12beta

--- src/default_legos/common.js
+++ src/default_legos/common.js
@@ -1,5 +1,5 @@
-import _ from 'lodash';
-import {reject, resolve} from "./promiseHandler";
+import _ from "lodash";
+import { reject, resolve } from "./promiseHandler";
 import q from "q";
 
 /**
@@ -11,11 +11,11 @@ import q from "q";
  * @returns {*} normalized locator that can be understood by nemo
  */
 function normalizedLocator(nemo, locator) {
-  if (typeof locator === 'object') {
+  if (typeof locator === "object") {
     return locator;
-  } else if (typeof locator === 'string' && locator.indexOf(':') > -1) {
+  } else if (typeof locator === "string" && locator.indexOf(":") > -1) {
     return locator;
-  } else if (typeof locator === 'string') {
+  } else if (typeof locator === "string") {
     return _.get(nemo, `view.${locator}By`, () => {})();
   }
 }
@@ -29,8 +29,8 @@ function normalizedLocator(nemo, locator) {
  * @param {number} waitTime waitTime in milliseconds by default it's 20000
  * @returns {Function}
  */
-function waitTillVanish(nemo, componentLocator, waitTime) {
-  waitTime = waitTime || _.get(nemo, 'data.waitTime', 20000);
+function waitTillVanish({ nemo, componentLocator, waitTime }) {
+  waitTime = waitTime || _.get(nemo, "data.waitTime", 20000);
   return function() {
     const locator = normalizedLocator(nemo, componentLocator);
     return nemo.driver.wait(
@@ -43,21 +43,19 @@ function waitTillVanish(nemo, componentLocator, waitTime) {
         });
       },
       waitTime,
-      `${componentLocator} wait failed`,
+      `${componentLocator} wait failed`
     );
   };
 }
 
-
 function waitVisible({ nemo, elementLocator, preCondition, waitTime } = {}) {
   preCondition = !(preCondition === false);
-  waitTime = waitTime || _.get(nemo, 'data.waitTime', 20000);
+  waitTime = waitTime || _.get(nemo, "data.waitTime", 20000);
   return function() {
     let locator;
     try {
       locator = normalizedLocator(nemo, elementLocator);
-    }
-    catch (ex) {
+    } catch (ex) {
       reject(defer, elementLocator);
     }
     var defer = q.defer();
@@ -68,16 +66,15 @@ function waitVisible({ nemo, elementLocator, preCondition, waitTime } = {}) {
   };
 }
 
-
-function goToUrl({nemo, url}) {
-    return function() {
-        return nemo.driver.get(url)
-    };
+function goToUrl({ nemo, url }) {
+  return function() {
+    return nemo.driver.get(url);
+  };
 }
 
 module.exports = {
   normalizedLocator: normalizedLocator,
   waitTillVanish: waitTillVanish,
   waitVisible: waitVisible,
-  goToUrl:goToUrl
+  goToUrl: goToUrl
 };
